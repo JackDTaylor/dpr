823bf06efc2b9adf559fe52d46e42c4ff87ecff4
Jack Taylor <msec.nt@gmail.com>
2019-04-02 07:48:12 +0300

    Update dpr.php

--- dpr.php
+++ dpr.php
@@ -1,102 +1,148 @@
 <?php
+/**
+ * dpr() is a debug function for extended and short `die(print_r($some_var, true));`
+ * @see https://github.com/JackDTaylor/dpr
+ */
 
-if(!function_exists('_dpr')) {
+if(!function_exists('_dpr') && !function_exists('is_developer')) {
 	/**
-	 * Use it for IPs you use for testing
-	 * @var string List of "|"-separated IPs
+	 * Declares IP addresses allowed to see the debug info.
+	 * For users not matching this filter dpr() will simply return first passed argument and do nothing.
+	 * @var string|boolean List of comma-separated IPs or false to disable this check
 	 */
 	if(!defined('DPR_DEVELOPER_IPS')) {
-		define('DPR_DEVELOPER_IPS', '127.0.0.1|192.168.0.1');
+		define('DPR_DEVELOPER_IPS', '127.0.0.1'); // Don't forget to change this to your IP or set it to false
 	}
 
 	/**
-	 * You can disable check for IPs using this constant
+	 * Whether show memory info or not
+	 * @var boolean Info will be shown only if DPR_SHOW_MEMINFO === true
 	 */
-	if(!defined('DPR_CHECK_DEVELOPER_IPS')) {
-		define('DPR_CHECK_DEVELOPER_IPS', true);
+	if(!defined('DPR_SHOW_MEMINFO')) {
+		define('DPR_SHOW_MEMINFO', true);
 	}
-	
+
+	/**
+	 * Default encoding to send with header('Content-Type: text/plain; charset=<your encoding here>');
+	 * @var boolean Info will be shown only if DPR_SHOW_MEMINFO === true
+	 */
+	if(!defined('DPR_ENCODING')) {
+		define('DPR_ENCODING', 'utf-8');
+	}
+
+	/**
+	 * If there's no $_SERVER['REQUEST_TIME_FLOAT'], then let's measure it at least somehow.
+	 */
+
+	if(!isset($_SERVER['REQUEST_TIME_FLOAT'])) {
+		$_SERVER['REQUEST_TIME_FLOAT'] = microtime(true);
+	}
+
 	/**
 	 * Checks whether user is developer or not
 	 * @return bool Returns TRUE if $_SERVER['REMOTE_ADDR'] is in DPR_DEVELOPER_IPS constant.
 	 */
 	function is_developer() {
-		if(!defined('DPR_DEVELOPER_IPS')) {
-			return false;
+		if(DPR_DEVELOPER_IPS === false) {
+			return true;
 		}
-
-		return in_array($_SERVER['REMOTE_ADDR'], explode('|', DPR_DEVELOPER_IPS));
+		return in_array(explode(',', DPR_DEVELOPER_IPS), $_SERVER['REMOTE_ADDR']);
 	}
 
 	/**
-	 * Internal function for _dpr()
+	 * Internal functions for _dpr()
 	 */
 	function __echo_null() { echo '[NULL]'; }
-
-	/**
-	 * Internal function for _dpr()
-	 * @param $variable
-	 */
 	function __echo_bool($variable) { echo $variable ? '[TRUE]' : '[FALSE]'; }
+
+	function __format_dpr_mem_number($val) {
+		$val = number_format($val / 1024 / 1024, 3, '.', '');
+		return str_pad($val, 13, ' ', STR_PAD_LEFT);
+	}
+	function __format_dpr_mem_percent($val) {
+		$val = number_format($val, 2, '.', '');
+		return str_pad($val, 6, ' ', STR_PAD_LEFT);
+	}
 
 	/**
 	 * Internal dpr() function.
-	 *
 	 * @param $variables   array       List of mixed values to print
 	 * @param $is_var_dump boolean     If true, then var_dump() will be used instead of print_r()
 	 * @param $breakpoint  string      Breakpoint location for dprb()/dprd() functions
-	 * @return mixed
+	 * @return
 	 */
 	function _dpr(array $variables = array(), $is_var_dump = false, $breakpoint = null) {
-		if(DPR_CHECK_DEVELOPER_IPS && is_developer() == false) {
-			return pos($variables);
+		if(is_developer() === false) {
+			return @pos($variables);
 		}
-
+		
 		if(ob_get_level()) {
 			while(ob_get_level()) {
 				ob_end_clean();
 			}
 		}
 
-
-		$html_mode = headers_sent();
-
-		if($html_mode === false) {
-			header('Content-type: text/plain; charset=utf-8');
+		if(!headers_sent()) {
+			header('Content-type: text/plain; charset=' . DPR_ENCODING);
 		} else {
-			echo '
-				<style>
-				* {
-					overflow: hidden; position: static;
-				}
-
-				#dprOutput {
-					position: fixed;
-					overflow: auto;
-					top: 0;
-					left: 0;
-					margin: 0;
-					padding: 5px;
-					box-sizing: border-box;
-					width: 100%;
-					height: 100%;
-					background-color: #FFFFFF;
-					color: #000000;
-					font-size: 14px;
-					line-height: 125%;
-					font-family: Courier New, monospace;
-					z-index: 2147483647;
-				}
-				</style>
-				<pre style="dprOutput">
-			';
+			echo '<style> * { overflow: hidden; position: static; } </style>';
+			echo '<pre style="position: fixed; overflow: auto; top: 0; left: 0; margin: 0; padding: 5px; box-sizing: border-box; width: 100%; height: 100%; background-color: #FFFFFF; color: #000000; font-size: 14px; line-height: 125%; font-family: Courier New; z-index: 2147483647">';
 		}
 
 		$called_at = debug_backtrace(false);
 		$called_at = $called_at[1];
 
 		// header
-		echo str_repeat('#', 78), PHP_EOL;
+		$exec_time = ((microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]) * 1000);
+		$exec_time = number_format($exec_time, 2, '.', '');
+
+		if(DPR_SHOW_MEMINFO === true) {
+			// Hey don't look at me that way!
+			// Oh, well, okay, whatever.
+			/** @FIXME Refactoring needed */
+			$mem_limit = trim(ini_get('memory_limit'));
+
+			switch(strtolower($mem_limit[strlen($mem_limit)-1])) {
+				case 'g': $mem_limit *= 1024;
+				case 'm': $mem_limit *= 1024;
+				case 'k': $mem_limit *= 1024;
+			}
+
+			$curr_emal = memory_get_usage();
+			$curr_real = memory_get_usage(true);
+			$peak_emal = memory_get_peak_usage();
+			$peak_real = memory_get_peak_usage(true);
+
+			$cep = __format_dpr_mem_percent($curr_emal / $mem_limit * 100) . '%';
+			$crp = __format_dpr_mem_percent($curr_real / $mem_limit * 100) . '%';
+			$pep = __format_dpr_mem_percent($peak_emal / $mem_limit * 100) . '%';
+			$prp = __format_dpr_mem_percent($peak_real / $mem_limit * 100) . '%';
+
+			$mem_limit = __format_dpr_mem_number($mem_limit) . 'M';
+
+			$curr_emal = __format_dpr_mem_number(memory_get_usage()) . 'M';
+			$curr_real = __format_dpr_mem_number(memory_get_usage(true)) . 'M';
+			$peak_emal = __format_dpr_mem_number(memory_get_peak_usage()) . 'M';
+			$peak_real = __format_dpr_mem_number(memory_get_peak_usage(true)) . 'M';
+
+
+			$mpem = str_pad($mpem, 17, ' ', STR_PAD_RIGHT);
+			$mprl = str_pad($mprl, 17, ' ', STR_PAD_RIGHT);
+
+			$internal_encoding = mb_internal_encoding();
+			mb_internal_encoding(DPR_ENCODING);
+
+			$s = chr(226).chr(148).chr(130);
+			echo " ____________________________________________________________________________ ", PHP_EOL;
+			echo "$s          $s Current usage $s    Peak usage $s  Memory limit $s   Curr $s   Peak $s", PHP_EOL;
+			echo "$s  emalloc $s{$curr_emal} $s{$peak_emal} $s{$mem_limit} $s{$cep} $s{$pep} $s", PHP_EOL;
+			echo "$s     real $s{$curr_real} $s{$peak_real} $s{$mem_limit} $s{$crp} $s{$prp} $s", PHP_EOL;
+			echo ' ', str_repeat(chr(194).chr(175), 76), ' ', PHP_EOL;
+
+			mb_internal_encoding($internal_encoding);
+		}
+
+		echo str_pad(" {$exec_time}ms", 76, '#', STR_PAD_LEFT), ' #', PHP_EOL;
 		echo '#', PHP_EOL;
 
 		if(is_null($breakpoint) == false) {
@@ -119,44 +165,32 @@ if(!function_exists('_dpr')) {
 			$function($variable);
 			echo PHP_EOL . PHP_EOL;
 		}
-
-		if($html_mode === true) {
-			echo '</pre>';
-		}
-
 		die();
 	}
 
 	/**
-	 * @deprecated
-	 * -s for "silent"
-	 * Executes dpr() only if is_developer() equals true.
-	 */
-	function dprs() {
-		_dpr(func_get_args());
-	}
-
-	/**
 	 * Basic functionality. Prints variables provided as arguments and stops the script execution.
-	 *
-	 * @internal param mixed $var1 Variable to print
-	 * @internal param mixed $_ [optional] Function supports any number of arguments
+	 * @param var1 mixed   Variable to print
+	 * @param _    mixed   [optional] Function supports any number of arguments
+	 * @return
 	 */
 	function dpr() {
-		_dpr(func_get_args());
+		return _dpr(func_get_args());
 	}
 
 	/**
 	 * Same as dpr(), but uses var_dump() instead of print_r()
 	 * @param var1 mixed   Variable to print
 	 * @param _    mixed   [optional] Function supports any number of arguments
+	 * @return
 	 */
 	function dprv() {
-		_dpr(func_get_args(), true);
+		return _dpr(func_get_args(), true);
 	}
 
 	/**
 	 * Prints backtrace and stops the script execution.
+	 * @return
 	 */
 	function dprt() {
 		$trace_result = array();
@@ -170,6 +204,7 @@ if(!function_exists('_dpr')) {
 
 	/**
 	 * Defines a breakpoint for dprd()
+	 * @return
 	 */
 	function dprb() {
 		$breakpoint_at = pos(debug_backtrace(false));
@@ -179,13 +214,42 @@ if(!function_exists('_dpr')) {
 
 	/**
 	 * Triggers dpr() if breakpoint was defined with dprb()
-	 *
-	 * @internal param mixed $var1 Variable to print
-	 * @internal param mixed $_ [optional] Function supports any number of arguments
+	 * @param var1 mixed   Variable to print
+	 * @param _    mixed   [optional] Function supports any number of arguments
+	 * @return
 	 */
 	function dprd() {
 		if(defined('__DPR_BREAKPOINT_POSITION')) {
-			_dpr(func_get_args(), false, __DPR_BREAKPOINT_POSITION);
+			return _dpr(func_get_args(), false, __DPR_BREAKPOINT_POSITION);
 		}
+	}
+
+	class dprmStorage {
+		static $storage = [];
+		static $locks = [];
+	}
+
+	function dprmFrom($key) {
+		if(isset(dprmStorage::$locks[$key])) {
+			return;
+		}
+
+		dprmStorage::$locks[$key] = microtime(true);
+	}
+
+	function dprmTo($key) {
+		if(!isset(dprmStorage::$locks[$key])) {
+			return;
+		}
+
+		dprmStorage::$storage[$key] = number_format((microtime(true) - dprmStorage::$locks[$key]) * 1000, 2, '.', '');
+
+		unset(dprmStorage::$locks[$key]);
+	}
+
+	function dprm(...$args) {
+		ksort(dprmStorage::$storage);
+
+		_dpr(array_merge([ dprmStorage::$storage ], $args));
 	}
 }
